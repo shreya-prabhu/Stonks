e457d36e1519c0f2e5cd05c4d6adbb272b9fae91
triambaka <triambakanaresh@ymail.com>
2021-04-11 11:59:27 +0530

    My Files

---
+++ app.py
@@ -0,0 +1,387 @@
+import sqlite3
+from flask import Flask, render_template,request,redirect,url_for,session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.secret_key = 'your secret key'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] ='root'
+app.config['MYSQL_DB'] = 'stonks'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+# Route for handling the login page logic
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     return render_template('login.html')
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login_admin():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM admin_profile WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Logged in successfully !'
+            session['loggedin'] = True
+            #session['id'] = account['id']
+            session['username'] = request.form['username']
+            session['username'] = account['username']
+            session['T_ID'] = 0
+            return render_template('admin_dashboard.html', msg = msg)
+
+        cursor.execute('SELECT * FROM client_profile WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['username'] = request.form['username']
+            return redirect("http://localhost:5000/dashboard", code=302)
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    return render_template('index.html')
+
+@app.route("/dashboard")
+def dashboard():
+    if session['loggedin'] == False:
+        return redirect("http://localhost:5000/login")
+    return render_template('dashboard.html')
+
+@app.route("/explorestocks")
+def explorestocks():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT SCode, CName, Price FROM stocks;')
+    stocklist = cursor.fetchall()
+    return render_template('explorestocks.html', stocks = stocklist)
+
+@app.route("/buy_stock")
+def buy_stock():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT SCode, Price FROM stocks;')
+    stocklist = cursor.fetchall()
+    return render_template('buy.html', stocks=stocklist)
+
+@app.route("/sell_stock")
+def sell_stock():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    cursor = mysql.connection.cursor()
+    username =session['username']
+    cursor.execute('SELECT stock_customer.SCode, quantity, Price FROM stock_customer INNER JOIN stocks ON stock_customer.SCode = stocks.SCode where stock_customer.CName = %s;',[username])
+    stocklist = cursor.fetchall()
+    print(stocklist)
+    return render_template('sell.html', stocks =stocklist)
+
+@app.route("/trade")
+def trade():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    return render_template('trade.html')
+
+@app.route("/transactions")
+def transactions():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    cursor = mysql.connection.cursor()
+    username = session['username']
+    cursor.execute(' SELECT S.CName from stocks S  left join transactions T on (T.Scode = S.Scode) where T.CName = %s;',[username])
+    companieslist = cursor.fetchall()
+    comp = []
+    for c in companieslist:
+        if c not in comp:
+            comp.append(c)
+    # companieslist = companieslist.values()
+    # comp = set(comp)
+    for company in comp:
+        company_name = company["CName"]
+        print("company",company_name)
+
+        cursor.execute('SELECT T.T_ID,T.CName, T.T_Time,T.T_Date,T.T_type,S.SCode,T.Quantity,S.CName as Company_Name,S.SDescription,S.Price, round(T.Quantity*S.Price,2) as total \
+            from stocks S  left join transactions T on (T.Scode = S.Scode) where T.CName=%s order by Company_Name',[username]);
+        transactionlist = cursor.fetchall()
+    return render_template('transactions.html', transactions = transactionlist, companies = comp)
+
+
+
+@app.route("/profile")
+def profile():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor()
+        username = session['username']
+        print("i am ", username)
+        cursor.execute('SELECT * FROM client_profile WHERE username = %s', [username])
+        account = cursor.fetchone()
+        return render_template("profile.html", account = account)
+    
+@app.route("/update_client", methods =['GET', 'POST'])
+def update_client():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    msg = ''
+    if 'loggedin' in session:
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            email = request.form['email']
+            phonenumber = request.form['phonenumber']
+            cursor = mysql.connection.cursor()
+            cursor.execute('SELECT * FROM client_profile WHERE username = %s', [username])
+            account = cursor.fetchone()
+            if account:
+                if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                    msg = 'Invalid email address !'
+                else:
+                    cursor.execute('UPDATE client_profile SET  username =%s, password =%s, email_id =%s,phone_no =%s WHERE username =% s',(username,password,email,phonenumber,session['username']  ))
+                    mysql.connection.commit()
+                    msg = 'You have successfully updated !'
+                    return redirect(url_for('dashboard'))
+        return render_template("update_client.html", msg = msg)
+    return redirect(url_for('login'))
+
+@app.route("/admin_profile")
+def admin_profile():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM admin_profile WHERE username = %s', (session['username'], ))
+        account = cursor.fetchone()
+        return render_template("admin_profile.html", account = account)
+
+@app.route("/Company")
+def company_1():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM CompanyDB')
+        account = cursor.fetchall()
+
+        return render_template("Company.html", account = account,len=len(account))
+
+    return redirect('login')
+#endpoint for search
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    if request.method == "POST":
+        companyname = request.form['companyname']
+        #companyname=string(companyname)
+        # search by author or book
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * from CompanyDB WHERE CName = %s', [companyname])
+        #cursor.commit()
+        data = cursor.fetchall()
+        # all in the search box will return all the tuples
+        if len(data) == 0 and companyname == 'all':
+            cursor.execute("SELECT * from CompanyDB")
+
+            data = cursor.fetchall()
+
+        return render_template('search.html', data=data)
+    return render_template('search.html')
+# end point for inserting data dynamicaly in the database
+@app.route('/insert', methods=['GET', 'POST'])
+def insert():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    if request.method == "POST":
+        cursor = mysql.connection.cursor()
+        CompanyName1 = request.form['CompanyName']
+        SecurityNo1 = request.form['SecurityNo']
+        Limited_Stock_Exchange1 = request.form['Limited_Stock_Exchange']
+
+        No_of_shares1 = request.form['No_of_shares']
+        cursor.execute('INSERT INTO CompanyDB VALUES (  %s, %s, %s, %s)', (CompanyName1, SecurityNo1, Limited_Stock_Exchange1, No_of_shares1, ))
+        mysql.connection.commit()
+        return redirect("http://localhost:5000/search", code=302)
+    return render_template('insert.html')
+@app.route("/Delete", methods=['GET', 'POST'])
+def delete():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    if request.method == "POST":
+        cursor = mysql.connection.cursor()
+        CompanyName1 = request.form['CompanyName']
+        cursor.execute('DELETE from CompanyDB WHERE CName = %s', [CompanyName1])
+        mysql.connection.commit()
+        return redirect("http://localhost:5000/Company", code=302)
+    return render_template('Delete.html')
+
+@app.route("/update", methods =['GET', 'POST'])
+def update():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    msg = ''
+    if 'loggedin' in session:
+        if request.method == 'POST' and 'oldCName' in request.form and 'CName' in request.form and 'SecurityNo' in request.form and 'Limited_Stock_Exchange' in request.form and 'No_of_shares' in request.form:
+            CName1 = request.form['oldCName']
+            CName2 = request.form['CName']
+            SecurityNo1 = request.form['SecurityNo']
+            Limited_Stock_Exchange1 = request.form['Limited_Stock_Exchange']
+
+            No_of_shares1 = request.form['No_of_shares']
+            cursor = mysql.connection.cursor()
+            cursor.execute('SELECT * FROM CompanyDB WHERE CName = % s', (CName1, ))
+            account = cursor.fetchone()
+            cursor.execute('UPDATE companydb SET  CName =% s, SecurityNo =% s, Limited_Stock_Exchange =% s, No_of_shares =% s WHERE CName =% s',(CName2, SecurityNo1, Limited_Stock_Exchange1, No_of_shares1,CName1))
+            mysql.connection.commit()
+            msg = 'You have successfully updated !'
+        elif request.method == 'POST':
+            msg = 'Please fill out the form !'
+        return render_template("update.html", msg = msg)
+    return render_template('update.html')
+
+@app.route('/client_insert', methods=['GET', 'POST'])
+def client_insert():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    if request.method == "POST":
+        cursor = mysql.connection.cursor()
+        fullname = request.form['fullname']
+        dob = request.form['dob']
+        email = request.form['email']
+        phonenumber = request.form['phonenumber']
+        username = request.form['username']
+        password = request.form['password']
+        aadharnumber = request.form['aadharnumber']
+        pannumber = request.form['pannumber']
+        securitycode = request.form['securitycode']
+        dpid = request.form['dpid']
+        bankacc = request.form['bankacc']
+        bankname= request.form['bankname']
+        bankifsc = request.form['bankifsc']
+        banktype = request.form['banktype']
+        cursor.execute('INSERT INTO client_profile VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', (fullname, dob, email, phonenumber, username, password, aadharnumber, pannumber, securitycode, dpid , bankacc))
+        mysql.connection.commit()
+        cursor.execute('INSERT INTO bank_details VALUES ( %s ,%s, %s, %s )' ,( bankname, bankacc, bankifsc, banktype))
+        mysql.connection.commit()
+        return redirect("http://localhost:5000/login", code=302)
+    return render_template('register.html')
+
+@app.route('/buy_check', methods=['GET', 'POST'])
+def buy_check():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    if request.method == "POST":
+        cursor = mysql.connection.cursor()
+        username = session['username']
+        print(session['username'])
+        num1 = request.form['num1']
+        num1 =int(num1)
+        stockid = request.form.get('name')
+        stockid= str(stockid)
+        cursor.execute('SELECT CName FROM stocks WHERE SCode = %s ', [stockid])
+        query1 = cursor.fetchone()
+        company= query1['CName']
+        cursor.execute('SELECT No_of_shares FROM CompanyDB WHERE CName = %s ', [company])
+        query2 = cursor.fetchone()
+        number= int(query2['No_of_shares'])
+        if num1<number:
+            num2= number-num1
+            cursor.execute('UPDATE CompanyDB SET No_of_shares = %s WHERE CName =%s;',(num2,company))
+            mysql.connection.commit()
+            query3=cursor.fetchone()
+            if query3:
+                num2 = int(query3['quantity'])
+                cursor.execute('UPDATE stock_customer SET quantity = %s WHERE CName =%s AND SCode =%s;',(num1+num2,username,stockid))
+            else:
+                cursor.execute('Insert into stock_customer values(%s,%s,%s)',(stockid,username,num1))
+            mysql.connection.commit()
+            #cursor.execute('SELECT T_ID FROM transactions')
+            #query4 = cursor.fetchall()
+            #if query4:
+            #    session['T_ID'] = int(query4[len(query4)-1]['T_ID'])+1
+            #    T_ID =session['T_ID']
+            #else:
+            #    T_ID = session['T_ID'] =0
+            #session['T_ID'] = session['T_ID']+1
+            dateTimeObj = datetime.now()
+            T_Time=str(dateTimeObj.hour)+':'+str(dateTimeObj.minute)+':'+str(dateTimeObj.second)+'.'+str(dateTimeObj.microsecond)
+            T_Date= str(dateTimeObj.year)+'-'+str(dateTimeObj.month)+'-'+str(dateTimeObj.day)
+            T_type ="Buy"
+            cursor.execute('Insert into transactions values(%s,%s,%s,%s,%s,%s,%s)',(0,username, T_Time, T_Date, T_type, stockid, num1))
+            mysql.connection.commit()
+            return redirect("http://localhost:5000/transactions", code=302)
+        return redirect("http://localhost:5000/dashboard", code=302)
+
+
+@app.route('/sell_check', methods=['GET', 'POST'])
+def sell_check():
+    if 'loggedin' not in session:
+        return redirect("http://localhost:5000/login")
+    if request.method == "POST":
+        cursor = mysql.connection.cursor()
+        username = session['username']
+        num1 = request.form['num1']
+        num1 =int(num1)
+        stockid = request.form.get('name')
+        stockid= str(stockid)
+        cursor.execute('SELECT quantity FROM stock_customer WHERE SCode = %s ', [stockid])
+        query1 = cursor.fetchone()
+        if query1:
+            quantity= int(query1['quantity'])
+            if(num1-quantity>0):
+                msg="Quantity is more than that bought"
+                return render_template("/dashboard.html", code=302,msg=msg)
+            cursor.execute('SELECT CName FROM stocks WHERE SCode = %s ', [stockid])
+            query1 = cursor.fetchone()
+            company= query1['CName']
+            cursor.execute('SELECT No_of_shares FROM CompanyDB WHERE CName = %s ', [company])
+            query2 = cursor.fetchone()
+            number= int(query2['No_of_shares'])
+            num2= number+num1
+            cursor.execute('UPDATE CompanyDB SET No_of_shares = %s WHERE CName =%s;',(num2,company))
+            mysql.connection.commit()
+            if(quantity-num1 !=0):
+                cursor.execute('UPDATE stock_customer SET quantity = %s WHERE CName =%s AND SCode =%s;',(quantity-num1,username,stockid))
+            else:
+                cursor.execute('DELETE from stock_customer where CName =%s AND SCode =%s;',(username,stockid))
+            mysql.connection.commit()
+            cursor.execute('SELECT T_ID FROM transactions')
+            query4 = cursor.fetchall()
+            """
+            if query4:
+                session['T_ID'] = int(query4[len(query4)-1]['T_ID'])+1
+                T_ID =session['T_ID']
+            else:
+                T_ID = session['T_ID'] =0
+            session['T_ID'] = session['T_ID']+1
+            """
+            dateTimeObj = datetime.now()
+            T_Time=str(dateTimeObj.hour)+':'+str(dateTimeObj.minute)+':'+str(dateTimeObj.second)+'.'+str(dateTimeObj.microsecond)
+            T_Date= str(dateTimeObj.year)+'-'+str(dateTimeObj.month)+'-'+str(dateTimeObj.day)
+            T_type ="Sell"
+            cursor.execute('Insert into transactions values(%s,%s,%s,%s,%s,%s,%s)',(0,username, T_Time, T_Date, T_type, stockid, num1))
+            mysql.connection.commit()
+            return redirect("http://localhost:5000/transactions", code=302)
+        return redirect("http://localhost:5000/dashboard", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,159 @@
+CREATE DATABASE if not exists stonks;
+USE stonks;
+DROP TABLE if exists admin_profile;
+DROP TABLE if exists CompanyDB;
+CREATE TABLE admin_profile(
+id int(11) NOT NULL UNIQUE,
+username VARCHAR(255)  NOT NULL,
+password VARCHAR(25) NOT NULL ,
+PRIMARY KEY(username));
+INSERT INTO admin_profile(id,username,password)
+VALUES
+("1","A","password");
+INSERT INTO admin_profile(id,username,password)
+VALUES
+("2","B@gmail.com","password");
+
+CREATE TABLE CompanyDB(
+CName VARCHAR(255)  NOT NULL UNIQUE,
+SecurityNo VARCHAR(50) NOT NULL UNIQUE ,
+Limited_Stock_Exchange  VARCHAR(255) NOT NULL,
+No_of_shares int(11) NOT NULL,
+PRIMARY KEY(CName));
+
+INSERT INTO CompanyDB(CName,SecurityNo,Limited_Stock_Exchange,No_of_shares)
+VALUES
+("Company1","9XGH12","NSE",10000),
+("Company2","9XGH13","BSE",10000),
+("Company3","9XGH14","BSE",10000),
+("Company4","9XGH15","NSE",10000);
+
+CREATE TABLE if not exists client_profile(
+FName VARCHAR(255)  NOT NULL,
+DOB date,
+email_id varchar(40) unique not null,
+phone_no varchar(10) unique not null,
+username varchar(15) unique NOT NULL,
+password varchar(20) not null,
+aadhar_no varchar(12) unique not null,
+pan_no varchar(10) unique not null,
+security_code varchar(20) not null,
+DP_ID varchar(10) not null,
+Bank_acc_no varchar(10) not null unique,
+PRIMARY KEY(username));
+
+INSERT INTO client_profile(FName,email_id,phone_no,username,password,aadhar_no,pan_no,security_code,
+DP_ID,Bank_acc_no)
+VALUES('moo','m@gmail.com',12,'moo','moo','1','1','1','1','1');
+
+INSERT INTO client_profile(FName,email_id,phone_no,username,password,aadhar_no,pan_no,security_code,
+DP_ID,Bank_acc_no)
+VALUES('boo','b@gmail.com',112,'boo','boo','2','2','2','2','2');
+
+
+Create table if not exists bank_details(
+BName Varchar(255) Not NULL,
+Bank_acc_no varchar(10) not null unique,
+bank_ifsc_no varchar(12) not null,
+account_type varchar(20) not null,
+PRIMARY KEY(Bank_acc_no),
+FOREIGN KEY (Bank_acc_no) REFERENCES client_profile(Bank_acc_no)
+ON UPDATE CASCADE ON DELETE CASCADE);
+
+Create table if not exists stocks(
+SCode varchar(10) Not Null Unique,
+CName VARCHAR(255)  NOT NULL UNIQUE,
+SDescription Varchar(255),
+Price float(2)  not null,
+Primary Key(SCode),
+FOREIGN KEY (CName) REFERENCES CompanyDB(CName)
+ON UPDATE CASCADE ON DELETE CASCADE);
+
+INSERT INTO stocks(SCode,CName,SDescription,Price) VALUES ('CT1','Company1','NULL',12.3);
+INSERT INTO stocks(SCode,CName,SDescription,Price) VALUES ('CT2','Company2','Market Rate',82.43);
+INSERT INTO stocks(SCode,CName,SDescription,Price) VALUES ('CT3','Company3','Market Rate',312.53);
+INSERT INTO stocks(SCode,CName,SDescription,Price) VALUES ('CT4','Company4','Market Rate',212.3);
+
+-- -- Create table if not exists transactions(
+-- -- T_ID varchar(5),
+-- -- CName VARCHAR(255)  NOT NULL,
+-- -- T_Time time,
+-- -- T_Date date,
+-- -- T_type varchar(10),
+-- -- SCode varchar(10) Not Null,
+-- -- Quantity integer,
+-- -- primary key(T_ID),
+-- -- foreign key(SCode) references stocks(SCode),
+-- -- FOREIGN KEY(CName) REFERENCES client_profile(username)
+-- -- ON UPDATE CASCADE ON DELETE CASCADE);
+
+-- -- INSERT INTO transactions(T_ID,CName,T_Time,T_Date,T_Type,SCode,Quantity) VALUES ('0','moo','12:12:12','2000-01-01','Buy','CT1',4);
+-- -- INSERT INTO transactions(T_ID,CName,T_Time,T_Date,T_Type,SCode,Quantity) VALUES ('1','moo','12:12:12','2000-01-01','Sell','CT1',4);
+-- -- INSERT INTO transactions(T_ID,CName,T_Time,T_Date,T_Type,SCode,Quantity) VALUES ('2','moo','12:12:12','2000-01-01','Buy','CT1',4);
+-- -- INSERT INTO transactions(T_ID,CName,T_Time,T_Date,T_Type,SCode,Quantity) VALUES ('3','boo','12:12:12','2000-01-01','Buy','CT2',4);
+-- -- INSERT INTO transactions(T_ID,CName,T_Time,T_Date,T_Type,SCode,Quantity) VALUES ('4','moo','12:12:12','2000-01-01','Buy','CT3',4);
+-- Create table if not exists stock_customer(
+-- SCode varchar(10) Not Null,
+-- CName VARCHAR(255)  NOT NULL,
+-- quantity integer not null,
+-- primary key(SCode, CName),
+-- foreign key(SCode) references stocks(SCode),
+-- FOREIGN KEY(CName) REFERENCES client_profile(username)
+-- ON UPDATE CASCADE ON DELETE CASCADE);
+
+-- Create table if not exists transactions(
+-- T_ID integer not null auto_increment,
+-- CName VARCHAR(255)  NOT NULL,
+-- T_Time time,
+-- T_Date date,
+-- T_type varchar(10),
+-- SCode varchar(10) Not Null,
+-- Quantity integer,
+-- primary key(T_ID),
+-- foreign key(SCode) references stocks(SCode),
+-- FOREIGN KEY(CName) REFERENCES client_profile(username)
+-- ON UPDATE CASCADE ON DELETE CASCADE);
+
+-- DELIMITER //
+-- CREATE TRIGGER display_stock_changes 
+-- BEFORE UPDATE ON CompanyDB FOR EACH ROW 
+-- BEGIN  
+-- 	IF OLD.No_of_shares <= 200 THEN 
+--     SIGNAL SQLSTATE '45000'
+-- 	SET MESSAGE_TEXT = 'Share not available';
+--     END IF;
+-- END;//
+-- DELIMITER ;
+
+Create table if not exists stock_customer(
+SCode varchar(10) Not Null,
+CName VARCHAR(255)  NOT NULL,
+quantity integer not null,
+primary key(SCode, CName),
+foreign key(SCode) references stocks(SCode),
+FOREIGN KEY(CName) REFERENCES client_profile(username)
+ON UPDATE CASCADE ON DELETE CASCADE);
+
+Create table if not exists transactions(
+T_ID integer not null auto_increment,
+CName VARCHAR(255)  NOT NULL,
+T_Time time,
+T_Date date,
+T_type varchar(10),
+SCode varchar(10) Not Null,
+Quantity integer,
+primary key(T_ID),
+foreign key(SCode) references stocks(SCode),
+FOREIGN KEY(CName) REFERENCES client_profile(username)
+ON UPDATE CASCADE ON DELETE CASCADE);
+
+DELIMITER //
+CREATE TRIGGER display_stock_changes 
+BEFORE UPDATE ON CompanyDB FOR EACH ROW 
+BEGIN  
+	IF OLD.No_of_shares <= 200 THEN 
+    SIGNAL SQLSTATE '45000'
+	SET MESSAGE_TEXT = 'Share not available';
+    END IF;
+END;//
+DELIMITER ;
